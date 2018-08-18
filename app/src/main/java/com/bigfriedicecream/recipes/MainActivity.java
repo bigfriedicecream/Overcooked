0532c689457956ce27970d1d6539ce0d81732bf9
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-08-18 17:41:02 +1000

    created main fragment

--- app/src/main/java/com/bigfriedicecream/recipes/MainActivity.java
+++ app/src/main/java/com/bigfriedicecream/recipes/MainActivity.java
@@ -3,11 +3,20 @@ package com.bigfriedicecream.recipes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bigfriedicecream.recipes.views.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.layout_app, new MainFragment())
+                    .commit();
+        }
     }
 }
