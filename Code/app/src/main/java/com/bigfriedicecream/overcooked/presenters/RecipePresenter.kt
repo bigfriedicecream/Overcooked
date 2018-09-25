e43aa4bcc59e84f8c1bdb5bf433ac7b03f71d0f1
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-09-25 19:40:43 +1000

    converted recipe presenter to kotlin

---
+++ Code/app/src/main/java/com/bigfriedicecream/overcooked/presenters/RecipePresenter.kt
@@ -0,0 +1,29 @@
+package com.bigfriedicecream.overcooked.presenters
+
+import com.bigfriedicecream.overcooked.interfaces.IRecipeContract
+import com.bigfriedicecream.overcooked.models.RecipeDataModel
+import com.bigfriedicecream.overcooked.observables.RecipeRepository
+
+import java.util.Observable
+import java.util.Observer
+
+class RecipePresenter(private val view:IRecipeContract.View) : IRecipeContract.Presenter, Observer {
+    private val recipeRepository:RecipeRepository = RecipeRepository.getInstance()
+
+    override fun start() {
+        recipeRepository.addObserver(this)
+    }
+
+    override fun stop() {
+        recipeRepository.deleteObserver(this)
+    }
+
+    override fun load(id: String) {
+        val recipe:RecipeDataModel = recipeRepository.get(id)
+        view.render(recipe)
+    }
+
+    override fun update(observable:Observable, o:Any) {
+
+    }
+}
