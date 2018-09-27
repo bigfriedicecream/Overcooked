d46c55c2834aa97e20b916d0fffceecc174e35e2
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-09-28 09:07:15 +1000

    added bootstrap

--- Tools/recipe-editor/src/App.js
+++ Tools/recipe-editor/src/App.js
@@ -24,48 +24,66 @@ class App extends Component {
                     const recipe = recipes[key];
                     return (
                         <div>
-                            <div>
-                                title:
-                                <input type="text" value={recipe.title} onChange={this.handleChange(key, 'title')} />
+                            <div class="form-group">
+                                <label>Title</label>
+                                <input type="text" className="form-control" value={recipe.title} onChange={this.handleChange(key, 'title')} />
                             </div>
-                            <div>
-                                serves:
-                                <input type="number" value={recipe.serves} onChange={this.handleChange(key, 'serves')} />
-                                makes:
-                                <input type="number" value={recipe.makes} onChange={this.handleChange(key, 'makes')} />
-                                prepTime:
-                                <input type="number" value={recipe.prepTime} onChange={this.handleChange(key, 'prepTime')} />
-                                cookTime:
-                                <input type="number" value={recipe.cookTime} onChange={this.handleChange(key, 'cookTime')} />
+                            <div class="form-group">
+                                <label>Serves</label>
+                                <input type="number" className="form-control" value={recipe.serves} onChange={this.handleChange(key, 'serves')} />
+                            </div>
+                            <div class="form-group">
+                                <label>Makes</label>
+                                <input type="number" className="form-control" value={recipe.makes} onChange={this.handleChange(key, 'makes')} />
+                            </div>
+                            <div class="form-group">
+                                <label>Prep Time</label>
+                                <input type="number" className="form-control" value={recipe.prepTime} onChange={this.handleChange(key, 'prepTime')} />
+                            </div>
+                            <div class="form-group">
+                                <label>Cook Time</label>
+                                <input type="number" className="form-control" value={recipe.cookTime} onChange={this.handleChange(key, 'cookTime')} />
                             </div>
                             <div>
                                 Ingredients
-                                {recipe.ingredients.map((ing, i) => {
+                                {/*recipe.ingredients.map((ing, i) => {
                                     return (
                                         <div key={`recipe-${key}ing-${i}`}>
-                                            <input type="number" value="1" />
-                                            <select>
-                                                <option value="op1">Option 1</option>
-                                                <option value="op2">Option 2</option>
-                                                <option value="op3">Option 3</option>
-                                            </select>
-                                            <input type="text" value="Recipe Display" />
+                                            <div class="form-group">
+                                                <label>Quantity</label>
+                                                <input type="number" className="form-control" value="1" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Ingredient Type</label>
+                                                <select className="form-control">
+                                                    <option>op1</option>
+                                                    <option>op2</option>
+                                                    <option>op3</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Recipe Display</label>
+                                                <input type="text" className="form-control" value="Recipe Display" />
+                                            </div>
                                         </div>
 
                                     )
-                                })}
+                                })*/}
                             </div>
-                            <div>
+                            {/*<div>
                                 Method
-                            </div>
+                            </div>*/}
                             <br /><hr /><br />
                         </div>
                     )
                 })}
                 <br />
-                <button>+</button>
+                <button type="button" className="btn btn-primary">+</button>
                 <br /><br /><br />
-                <input type="text" value={JSON.stringify(this.state.data)} />
+                <div class="form-group">
+                    <label>Output</label>
+                    <input type="text" className="form-control" value={JSON.stringify(this.state.data)} />
+                </div>
             </div>
         );
     }
