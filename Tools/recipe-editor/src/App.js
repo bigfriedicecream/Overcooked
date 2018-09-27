a215249e5ea3f64fe31177de59f2b0941f64ba84
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-09-27 21:09:36 +1000

    added recipe editor fields

--- Tools/recipe-editor/src/App.js
+++ Tools/recipe-editor/src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import data from './recipes.json'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import data from './recipes.json';
+import AddIcon from '@material-ui/icons/Add';
 
 class App extends Component {
     constructor(props) {
@@ -23,12 +30,31 @@ class App extends Component {
         return (
             <div className="App">
                 {Object.keys(recipes).map(key => {
+                    const recipe = recipes[key];
                     return (
-                        <TextField key={key} label="Title" margin="normal" fullWidth value={recipes[key].title} onChange={this.handleChange(key, 'title')} />
+                        <ExpansionPanel key={key}>
+                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                <Typography>{recipe.title}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails className="flex-">
+                                <Grid container spacing={24}>
+                                    <Grid item xs={12}>
+                                        <TextField label="Title" fullWidth margin="normal" value={recipe.title} onChange={this.handleChange(key, 'title')} />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField label="Serves" margin="normal" value={recipe.serves} onChange={this.handleChange(key, 'serves')} type="number" />
+                                        <TextField label="Makes" margin="normal" value={recipe.makes} onChange={this.handleChange(key, 'makes')} type="number" />
+                                        <TextField label="Prep Time" margin="normal" value={recipe.prepTime} onChange={this.handleChange(key, 'prepTime')} type="number" />
+                                        <TextField label="Cook Time" margin="normal" value={recipe.cookTime} onChange={this.handleChange(key, 'cookTime')} type="number" />
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
                     )
                 })}
-                <Button variant="contained" color="primary">Save</Button>
-                <br /><br />
+                <br />
+                <Button variant="contained" color="primary"><AddIcon /></Button>
+                <br /><br /><br />
                 <TextField label="Output" margin="normal" value={JSON.stringify(this.state.data)} variant="outlined" />
             </div>
         );
