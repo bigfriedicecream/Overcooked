e2af256fe2460a8055da6895b947c1407f70d418
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-09-28 11:41:12 +1000

    added lookups and modified ingredient display

--- Tools/recipe-editor/src/Recipe/Ingredient.jsx
+++ Tools/recipe-editor/src/Recipe/Ingredient.jsx
@@ -1,42 +1,34 @@
 import React from 'react';
+import { LookupIngType } from '../Lookups/LookupIngType'
 
-const Ingredient = ({ quantity, unitType, ingredientType, display }) => {
+const Ingredient = ({ ingredientType, quantity, ingredient, display }) => {
     return (
         <div className="d-flex">
             <div className="form-group">
                 <label>Ingredient Type</label>
                 <select className="form-control" value={ingredientType}>
-                    <option value={0}>Normal</option>
-                    <option value={1}>Heading</option>
-                    <option value={2}>Text Only</option>
+                    {LookupIngType.dataList().map(item => {
+                        const { id, description } = item
+                        return (
+                            <option key={`lookupingtype-${id}`} value={id}>{description}</option>
+                        )
+                    })}
                 </select>
             </div>
-            <div className="form-group">
-                <label>Quantity</label>
-                <input type="number" className="form-control" value={quantity} />
-            </div>
-            <div className="form-group">
-                <label>Unit Type</label>
-                <select className="form-control" value={unitType}>
-                    <option value={0}>Singular</option>
-                    <option value={1}>Grams</option>
-                    <option value={2}>Millilitres</option>
-                    <option value={3}>Tbsp</option>
-                    <option value={4}>tsp</option>
-                    <option value={5}>Cups</option>
-                    <option value={6}>Bunch</option>
-                    <option value={7}>Rashers</option>
-                    <option value={8}>Head</option>
-                    <option value={9}>Sprig</option>
-                    <option value={10}>Stalk</option>
-                    <option value={11}>Sheets</option>
-                    <option value={12}>Slice</option>
-                </select>
-            </div>
-            <div className="form-group">
-                <label>Display</label>
-                <input type="text" className="form-control" value={display} />
-            </div>
+            {
+                ingredientType === LookupIngType.normal.id ? 
+                    <div className="form-group">
+                        <label>Quantity</label>
+                        <input type="number" className="form-control" value={quantity} />
+                    </div> : ''
+            }
+            {
+                ingredientType === LookupIngType.heading.id || ingredientType === LookupIngType.textOnly.id ?
+                    <div className="form-group">
+                        <label>Display</label>
+                        <input type="text" className="form-control" value={display} />
+                    </div> : ''
+            }
         </div>
     )
 }
