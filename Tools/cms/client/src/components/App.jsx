70dfb04fca425b0ae8ed386fff48e26ade463342
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-10-02 22:21:49 +1000

    added navbar

--- Tools/cms/client/src/components/App.jsx
+++ Tools/cms/client/src/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Root from './Root';
 import RecipeList from './recipe/RecipeList';
 import IngredientList from './ingredient/IngredientList';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -44,7 +45,15 @@ class App extends Component {
 
     onRecipeFieldChange = (id, field) => e => {
         const data = Object.assign({}, this.state.data);
-        data.recipes[id][field] = e.target.value
+
+        var targetValue = e.target.value;
+
+        if (e.target.type === 'number') {
+            targetValue = parseFloat(e.target.value)
+        }
+
+        data.recipes[id][field] = targetValue
+
         this.setState({ data });
     }
 
@@ -53,10 +62,19 @@ class App extends Component {
             onRecipeFieldChange: this.onRecipeFieldChange
         }
 
+        // <button type="button" className="btn btn-primary" onClick={this.onSave}>Save</button>
+
         return (
             <div>
-                <button type="button" className="btn btn-primary" onClick={this.onSave}>Save</button>
-                <hr />
+                <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+                    <div className="navbar-nav">
+                        <Link to={'/recipe/list'} className="nav-link">Recipe list</Link>
+                        <Link to={'/ingredient/list'} className="nav-link">Ingredient list</Link>
+                    </div>
+                    <div className="navbar-nav ml-auto">
+                        <button type="button" className="btn btn-primary" onClick={this.onSave}>Save</button>
+                    </div>
+                </nav>
                 <Switch>
                     <Route exact path='/' component={Root} />
                     <Route path='/recipe/list' render={routeProps => <RecipeList {...routeProps} data={this.state.data} handlers={handlers} />} />
