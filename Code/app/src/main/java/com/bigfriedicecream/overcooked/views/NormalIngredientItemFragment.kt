184126e9d0bf56935ca932cf5baa79eebe809935
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-10-22 21:42:54 +1100

    fixed ingredient unit types

--- Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
+++ Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.bigfriedicecream.overcooked.R
+import com.bigfriedicecream.overcooked.lookups.LookupIngUnitType
 import com.bigfriedicecream.overcooked.models.NormalRecipeIngredient
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_textonly_ingredient_item.view.*
@@ -20,8 +21,15 @@ class NormalIngredientItemFragment:Fragment() {
         val data:String? = arguments?.getString("data")
 
         if (data != null) {
-            val recipeIngredient:NormalRecipeIngredient = Gson().fromJson(data, NormalRecipeIngredient::class.java)
-            view.item.text = recipeIngredient.name
+            val ing:NormalRecipeIngredient = Gson().fromJson(data, NormalRecipeIngredient::class.java)
+
+            val ingUnitType:LookupIngUnitType? = LookupIngUnitType.dataLookup(ing.ingUnitTypeId)
+            val name:String = if (ing.quantity > 1) ing.namePlural else ing.name
+            val unit:String? = if (ing.quantity > 1) ingUnitType?.shortNamePlural else ingUnitType?.shortName
+
+            val displayResult = "${ing.quantity}$unit$name"
+
+            view.item.text = displayResult
         }
 
         return view
