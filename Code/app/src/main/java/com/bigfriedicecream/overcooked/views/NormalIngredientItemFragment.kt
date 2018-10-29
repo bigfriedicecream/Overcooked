d90043bc3f9ca6f93bd54e8b6037e3c065b36135
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-10-29 21:34:35 +1100

    calculated ing quantities

--- Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
+++ Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import com.bigfriedicecream.overcooked.R
 import com.bigfriedicecream.overcooked.lookups.LookupIngUnitType
 import com.bigfriedicecream.overcooked.models.NormalRecipeIngredient
+import com.bigfriedicecream.overcooked.models.UnitData
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_textonly_ingredient_item.view.*
 
@@ -20,14 +21,29 @@ class NormalIngredientItemFragment:Fragment() {
 
         val data:String? = arguments?.getString("data")
 
+        val multiplier = 1
+
         if (data != null) {
             val ing:NormalRecipeIngredient = Gson().fromJson(data, NormalRecipeIngredient::class.java)
 
-            val ingUnitType:LookupIngUnitType? = LookupIngUnitType.dataLookup(ing.ingUnitTypeId)
-            val name:String = if (ing.quantity > 1) ing.namePlural else ing.name
-            val unit:String? = if (ing.quantity > 1) ingUnitType?.shortNamePlural else ingUnitType?.shortName
+            // primary unit
+            val primaryUnitType = LookupIngUnitType.dataLookup(ing.ingUnitTypeId)
+            val primaryQuantity = ing.quantity * multiplier
+            val primaryUnit = if (ing.quantity > 1) primaryUnitType?.shortNamePlural else primaryUnitType?.shortName
 
-            val displayResult = "${ing.quantity}$unit$name"
+            // alternate unit
+            var alternateString = ""
+            if (ing.alternateUnit != 0) {
+                val alternateUnitType = LookupIngUnitType.dataLookup(ing.alternateUnit)
+                val alternateUnitData = ing.unitTypes.find { unitData -> unitData.id == ing.alternateUnit }
+                val alternateQuantity = if (alternateUnitData != null) alternateUnitData.ratio * ing.quantity * multiplier else 0.0
+                val alternateIngName = if (alternateQuantity > 1) alternateUnitType?.shortNamePlural?.trimEnd() else alternateUnitType?.shortName?.trimEnd()
+                alternateString = "($alternateQuantity$alternateIngName) "
+            }
+
+            val ingName = if (ing.quantity > 1) ing.namePlural else ing.name
+
+            val displayResult = "$primaryQuantity$primaryUnit$alternateString$ingName"
 
             view.item.text = displayResult
         }
