067b4ee0f9f15b19a4b3f87f4a5fdfffa69af04c
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-10-30 18:32:25 +1100

    added fraction extension

--- Code/app/src/main/java/com/bigfriedicecream/overcooked/utils/Extensions.kt
+++ Code/app/src/main/java/com/bigfriedicecream/overcooked/utils/Extensions.kt
@@ -1,7 +1,22 @@
 package com.bigfriedicecream.overcooked.utils
 
+import kotlin.math.roundToInt
+
 fun Int.Companion.minsToPrettyTimeFormat(mins:Int):String {
     val hours:Int = mins / 60
     val leftoverMins:Int = mins % 60
     return if (hours > 0) "${hours}H ${leftoverMins}MIN" else "${mins}MIN"
+}
+
+fun Double.Companion.fractionFromNumber(value:Double):String {
+    val intPart = value.toInt()
+    val fractionPart = value - intPart
+
+    if (fractionPart > 12.5 && fractionPart < 29.0) return (if (intPart > 0) "$intPart " else "") + "1/4"
+    if (fractionPart > 29.0 && fractionPart < 41.5) return (if (intPart > 0) "$intPart " else "") + "1/3"
+    if (fractionPart > 41.5 && fractionPart < 58.0) return (if (intPart > 0) "$intPart " else "") + "1/2"
+    if (fractionPart > 58.0 && fractionPart < 70.5) return (if (intPart > 0) "$intPart " else "") + "2/3"
+    if (fractionPart > 70.5 && fractionPart < 87.5) return (if (intPart > 0) "$intPart " else "") + "3/4"
+
+    return value.roundToInt().toString()
 }
--- Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
+++ Code/app/src/main/java/com/bigfriedicecream/overcooked/views/NormalIngredientItemFragment.kt
@@ -10,6 +10,7 @@ import com.bigfriedicecream.overcooked.R
 import com.bigfriedicecream.overcooked.lookups.LookupIngUnitType
 import com.bigfriedicecream.overcooked.models.NormalRecipeIngredient
 import com.bigfriedicecream.overcooked.models.UnitData
+import com.bigfriedicecream.overcooked.utils.fractionFromNumber
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_textonly_ingredient_item.view.*
 
@@ -28,7 +29,7 @@ class NormalIngredientItemFragment:Fragment() {
 
             // primary unit
             val primaryUnitType = LookupIngUnitType.dataLookup(ing.ingUnitTypeId)
-            val primaryQuantity = ing.quantity * multiplier
+            val primaryQuantity = Double.fractionFromNumber(ing.quantity * multiplier)
             val primaryUnit = if (ing.quantity > 1) primaryUnitType?.shortNamePlural else primaryUnitType?.shortName
 
             // alternate unit
@@ -37,8 +38,9 @@ class NormalIngredientItemFragment:Fragment() {
                 val alternateUnitType = LookupIngUnitType.dataLookup(ing.alternateUnit)
                 val alternateUnitData = ing.unitTypes.find { unitData -> unitData.id == ing.alternateUnit }
                 val alternateQuantity = if (alternateUnitData != null) alternateUnitData.ratio * ing.quantity * multiplier else 0.0
+                val alternateQuantityString = Double.fractionFromNumber(alternateQuantity)
                 val alternateIngName = if (alternateQuantity > 1) alternateUnitType?.shortNamePlural?.trimEnd() else alternateUnitType?.shortName?.trimEnd()
-                alternateString = "($alternateQuantity$alternateIngName) "
+                alternateString = "($alternateQuantityString$alternateIngName) "
             }
 
             val ingName = if (ing.quantity > 1) ing.namePlural else ing.name
