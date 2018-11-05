cc50365fc0151368c58502e621a648fcba98961a
bigfriedicecream <michaeldsouza.md@gmail.com>
2018-11-05 22:10:57 +1100

    added base basil chicken recipe

--- Tools/cms/client/src/components/ingredient/IngredientList.jsx
+++ Tools/cms/client/src/components/ingredient/IngredientList.jsx
@@ -5,13 +5,20 @@ const IngredientList = ({ data, handlers }) => {
     if (data === null) return null
 
     const { ingredients } = data
+    const sortedIngredients = Object.keys(ingredients).sort((a,b) => {
+        const aName = ingredients[a].name.toLowerCase();
+        const bName = ingredients[b].name.toLowerCase();
+        if (aName < bName) { return -1; }
+        if (aName > bName) { return 1; }
+        return 0;
+    })
 
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-xl">
                     <ul className="list-group">
-                        {Object.keys(ingredients).map(key => {
+                        {sortedIngredients.map(key => {
                             const ingredient = ingredients[key];
                             return (
                                 <li key={`ingredient-${ingredient.id}`} className="list-group-item">
