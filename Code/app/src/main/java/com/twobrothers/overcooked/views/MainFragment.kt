c3c27467141ab48620395cec05f729c0df826903
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-02-09 21:24:29 +1100

    added retrofit2

--- Code/app/src/main/java/com/twobrothers/overcooked/views/MainFragment.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/views/MainFragment.kt
@@ -11,66 +11,9 @@ import com.twobrothers.overcooked.R
 import com.twobrothers.overcooked.interfaces.IMainContract
 import com.twobrothers.overcooked.presenters.MainPresenter
 
-class MainFragment : Fragment(), IMainContract.View {
-
-    private var presenter:IMainContract.Presenter? = null
-    private var isLoaded:Boolean = false
+class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?): View? {
-        presenter = MainPresenter(this)
-
-        if (state != null) {
-            isLoaded = state.getBoolean("isLoaded")
-        }
-
         return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        presenter?.start()
-        if (!isLoaded) {
-            presenter?.load()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        presenter?.stop()
-    }
-
-    override fun onSaveInstanceState(state: Bundle) {
-        super.onSaveInstanceState(state)
-        state.putBoolean("isLoaded", isLoaded)
-    }
-
-    override fun renderList() {
-        val fragmentManager = fragmentManager
-        fragmentManager!!.popBackStack("root", FragmentManager.POP_BACK_STACK_INCLUSIVE)
-
-        getFragmentManager()!!
-                .beginTransaction()
-                .replace(R.id.layout_main, RecipeListFragment())
-                .addToBackStack("root")
-                .commit()
-
-        isLoaded = true
-    }
-
-    override fun renderRecipe(id: String) {
-        val recipeFragment = RecipeFragment()
-
-        val bundle = Bundle()
-        bundle.putString("id", id)
-
-        recipeFragment.arguments = bundle
-
-        fragmentManager!!
-                .beginTransaction()
-                .replace(R.id.layout_main, recipeFragment)
-                .addToBackStack(null)
-                .commit()
-
-        isLoaded = true
     }
 }
