3ea94d832cc8c17c58081a5c4b40d39c26611648
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-02-13 19:50:16 +1100

    added retrofit builder

--- Code/app/src/main/java/com/twobrothers/overcooked/views/RecipeListFragment.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/views/RecipeListFragment.kt
@@ -11,13 +11,23 @@ import com.twobrothers.overcooked.interfaces.IRecipeListContract
 import com.twobrothers.overcooked.models.RecipeModel
 import com.twobrothers.overcooked.presenters.RecipeListPresenter
 import com.google.gson.Gson
+import com.twobrothers.overcooked.BuildConfig
+import io.reactivex.plugins.RxJavaPlugins
 import kotlinx.android.synthetic.main.fragment_recipe_list.view.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 
 import java.util.HashMap
+
+
 
 class RecipeListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.API_ENDPOINT)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
         return inflater.inflate(R.layout.fragment_recipe_list, container, false)
     }
 }
