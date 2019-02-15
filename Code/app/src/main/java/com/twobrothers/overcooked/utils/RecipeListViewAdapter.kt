615d8fb217cc5a0ce6a9c46b992bfd35c9f5bb8c
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-02-16 10:14:37 +1100

    creating recipe list adapter for recycle view

---
+++ Code/app/src/main/java/com/twobrothers/overcooked/utils/RecipeListViewAdapter.kt
@@ -0,0 +1,27 @@
+package com.twobrothers.overcooked.utils
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.twobrothers.overcooked.R
+
+class RecipeListViewAdapter(private val dataset:Array<String>):RecyclerView.Adapter<RecipeListViewAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
