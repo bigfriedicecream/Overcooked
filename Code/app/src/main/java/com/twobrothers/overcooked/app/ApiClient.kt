f0878dbc3f0bf1934ca864894c57941cee0acce3
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-02-23 11:03:47 +1100

    caching the recipe list request

--- Code/app/src/main/java/com/twobrothers/overcooked/app/ApiClient.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/app/ApiClient.kt
@@ -1,10 +1,15 @@
 package com.twobrothers.overcooked.app
 
+import android.content.Context
+import android.preference.PreferenceManager
+import com.google.gson.Gson
 import com.twobrothers.overcooked.BuildConfig
+import com.twobrothers.overcooked.Overcooked
 import com.twobrothers.overcooked.models.recipelist.RecipeListModel
-import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -19,9 +24,25 @@ object ApiClient {
             .create(ApiService::class.java)
 
     fun getRecipes(): Single<RecipeListModel> {
-        return apiService
+        val pref = PreferenceManager.getDefaultSharedPreferences(Overcooked.appContext)
+        val recipeListCache = pref.getString("recipeListCache", "")
+        val mDisposable = CompositeDisposable()
+
+        println("in cache: $recipeListCache")
+
+        val request = apiService
                 .getRecipes()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+
+        mDisposable.add(
+            request.subscribeBy(
+                onSuccess = {
+                    pref.edit().putString("recipeListCache", Gson().toJson(it)).apply()
+                    mDisposable.dispose()
+                }
+        ))
+
+        return request
     }
 }
