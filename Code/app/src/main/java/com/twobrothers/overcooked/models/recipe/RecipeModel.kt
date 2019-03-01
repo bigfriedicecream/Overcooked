cf35024e7f14c4a9a0f6d595bcd28467f9b23510
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-03-02 09:43:57 +1100

    added ingredient section to recipe model

--- Code/app/src/main/java/com/twobrothers/overcooked/models/recipe/RecipeModel.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/models/recipe/RecipeModel.kt
@@ -4,5 +4,21 @@ class RecipeModel {
     val id: String = ""
     val title: String = ""
     val imageUrl: String = ""
+    val prepTime: Int = 0
+    val cookTime: Int = 0
     val method: ArrayList<String> = ArrayList()
+    val ingredientSections: ArrayList<IngredientSection> = ArrayList()
+
+    class IngredientSection {
+        val ingredientType: Int = 0
+
+        class Quantified {
+            val amount: Int = 0
+            val unitIds: ArrayList<Int> = ArrayList()
+            val foodId: String = ""
+        }
+        class FreeText {
+            val description: String = ""
+        }
+    }
 }
