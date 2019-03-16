e044de3cd1dd948f91f46dae4cfdc6631d133e27
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-03-16 20:28:35 +1100

    created custom okhttp cache

--- Code/app/src/main/java/com/twobrothers/overcooked/app/ApiClient.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/app/ApiClient.kt
@@ -1,24 +1,57 @@
 package com.twobrothers.overcooked.app
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import com.google.gson.*
 import com.twobrothers.overcooked.BuildConfig
+import com.twobrothers.overcooked.Overcooked
 import com.twobrothers.overcooked.lookups.LookupIngredientType
 import com.twobrothers.overcooked.models.recipe.RecipeModel
 import com.twobrothers.overcooked.models.recipelist.RecipeListModel
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
+    private const val cacheSize = (5 * 1024 * 1024).toLong() // 5MB
+    private val myCache = Cache(Overcooked.appContext!!.cacheDir, cacheSize)
+
+    private val okHttpClient = OkHttpClient.Builder()
+            .cache(myCache)
+            .addInterceptor {
+                var request = it.request()
+                request = if (hasNetwork()!!)
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 60).build() // 60 seconds
+                else
+                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build() // 7 days
+                it.proceed(request)
+            }
+            .build()
+
+
     private val apiService = Retrofit.Builder()
             .baseUrl(BuildConfig.API_ENDPOINT)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(ApiService::class.java)
+
+    private fun hasNetwork(): Boolean? {
+        var isConnected: Boolean? = false
+        val connectivityManager = Overcooked.appContext?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected) {
+            isConnected = true
+        }
+        return isConnected
+    }
 
     fun getRecipes(): Single<RecipeListModel> {
         return apiService
