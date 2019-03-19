2df78608e054f8d6559524564117999499006e2a
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-03-19 20:54:23 +1100

    improved fraction spacing

--- Code/app/src/main/java/com/twobrothers/overcooked/utils/Extensions.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/utils/Extensions.kt
@@ -10,11 +10,11 @@ fun Double.Companion.toFraction(value:Double):String {
     val intPart = value.toInt()
     val fractionPart = value - intPart
 
-    if (fractionPart > 0.125 && fractionPart < 0.29) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>4</sub>")
-    if (fractionPart > 0.29 && fractionPart < 0.415) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>3</sub>")
-    if (fractionPart > 0.415 && fractionPart < 0.58) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>2</sub>")
-    if (fractionPart > 0.58 && fractionPart < 0.705) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>2</sup>&frasl;<sub>3</sub>")
-    if (fractionPart > 0.705 && fractionPart < 0.875) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>3</sup>&frasl;<sub>4</sub>")
+    if (fractionPart > 0.125 && fractionPart < 0.29) return (if (intPart > 0) "$intPart" else "") + String.fromHtml("&#65279;<sup>1</sup>&frasl;<sub>4</sub>")
+    if (fractionPart > 0.29 && fractionPart < 0.415) return (if (intPart > 0) "$intPart" else "") + String.fromHtml("&#65279;<sup>1</sup>&frasl;<sub>3</sub>")
+    if (fractionPart > 0.415 && fractionPart < 0.58) return (if (intPart > 0) "$intPart" else "") + String.fromHtml("&#65279;<sup>1</sup>&frasl;<sub>2</sub>")
+    if (fractionPart > 0.58 && fractionPart < 0.705) return (if (intPart > 0) "$intPart" else "") + String.fromHtml("&#65279;<sup>2</sup>&frasl;<sub>3</sub>")
+    if (fractionPart > 0.705 && fractionPart < 0.875) return (if (intPart > 0) "$intPart" else "") + String.fromHtml("&#65279;<sup>3</sup>&frasl;<sub>4</sub>")
 
     return value.roundToInt().toString()
 }
