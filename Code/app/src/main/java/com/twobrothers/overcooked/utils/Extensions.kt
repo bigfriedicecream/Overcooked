a688fb407fa577cec13f21b3275d64745f4fd776
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-03-19 19:53:46 +1100

    displaying fractions as html characters

--- Code/app/src/main/java/com/twobrothers/overcooked/utils/Extensions.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/utils/Extensions.kt
@@ -10,11 +10,11 @@ fun Double.Companion.toFraction(value:Double):String {
     val intPart = value.toInt()
     val fractionPart = value - intPart
 
-    if (fractionPart > 0.125 && fractionPart < 0.29) return (if (intPart > 0) "$intPart " else "") + "1/4"
-    if (fractionPart > 0.29 && fractionPart < 0.415) return (if (intPart > 0) "$intPart " else "") + "1/3"
-    if (fractionPart > 0.415 && fractionPart < 0.58) return (if (intPart > 0) "$intPart " else "") + "1/2"
-    if (fractionPart > 0.58 && fractionPart < 0.705) return (if (intPart > 0) "$intPart " else "") + "2/3"
-    if (fractionPart > 0.705 && fractionPart < 0.875) return (if (intPart > 0) "$intPart " else "") + "3/4"
+    if (fractionPart > 0.125 && fractionPart < 0.29) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>4</sub>")
+    if (fractionPart > 0.29 && fractionPart < 0.415) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>3</sub>")
+    if (fractionPart > 0.415 && fractionPart < 0.58) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>1</sup>&frasl;<sub>2</sub>")
+    if (fractionPart > 0.58 && fractionPart < 0.705) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>2</sup>&frasl;<sub>3</sub>")
+    if (fractionPart > 0.705 && fractionPart < 0.875) return (if (intPart > 0) "$intPart " else "") + String.fromHtml("<sup>3</sup>&frasl;<sub>4</sub>")
 
     return value.roundToInt().toString()
 }
