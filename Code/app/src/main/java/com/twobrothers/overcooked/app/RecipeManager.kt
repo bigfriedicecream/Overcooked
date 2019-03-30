ac73f3c98c0e3f1e0bfdc35ed9b3b03bd3503e2e
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-03-30 13:50:01 +1100

    simplified recipe response model to recipe conversion

--- Code/app/src/main/java/com/twobrothers/overcooked/app/RecipeManager.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/app/RecipeManager.kt
@@ -11,37 +11,8 @@ object RecipeManager {
 
     fun getRecipe(id: String): Single<RecipeModel> {
         val recipeCache = CacheService.get("recipe-$id", object: TypeToken<CacheItem<RecipeResponseModel.Recipe>>(){}.type, RecipeResponseModel.Recipe::class.java)
-        val request = ApiClient.getRecipe(id)
-                .map {
-                    CacheService.put("recipe-${it.data.recipe.id}", it.data.recipe, 1000 * 60)
-                    val recipe = it.data.recipe
-                    val ingredients = ArrayList<RecipeModel.Ingredient>()
 
-                    recipe.ingredientSections.forEach {
-                        if (!it.heading.isNullOrBlank()) {
-                            ingredients.add(RecipeModel.Heading(LookupIngredientType.Heading.id, it.heading))
-                        }
-                    }
-
-                    RecipeModel(
-                            recipe.id,
-                            recipe.title,
-                            recipe.prepTime,
-                            recipe.cookTime,
-                            ingredients,
-                            recipe.method,
-                            recipe.referenceUrl,
-                            recipe.imageUrl,
-                            recipe.lastUpdated,
-                            recipe.serves,
-                            recipe.makes
-                    )
-                }
-
-        recipeCache ?: return request
-
-        if (recipeCache.isFresh()) {
-            val recipe = recipeCache.data
+        fun getRecipeModelFromResponse(recipe: RecipeResponseModel.Recipe): RecipeModel {
             val ingredients = ArrayList<RecipeModel.Ingredient>()
 
             recipe.ingredientSections.forEach {
@@ -50,51 +21,42 @@ object RecipeManager {
                 }
             }
 
+            return RecipeModel(
+                    recipe.id,
+                    recipe.title,
+                    recipe.prepTime,
+                    recipe.cookTime,
+                    ingredients,
+                    recipe.method,
+                    recipe.referenceUrl,
+                    recipe.imageUrl,
+                    recipe.lastUpdated,
+                    recipe.serves,
+                    recipe.makes
+            )
+        }
+
+        val request = ApiClient.getRecipe(id)
+                .map {
+                    CacheService.put("recipe-${it.data.recipe.id}", it.data.recipe, 1000 * 60)
+                    getRecipeModelFromResponse(it.data.recipe)
+                }
+
+        recipeCache ?: return request
+
+        if (recipeCache.isFresh()) {
             return Single.create {
                 it.onSuccess(
-                        RecipeModel(
-                                recipe.id,
-                                recipe.title,
-                                recipe.prepTime,
-                                recipe.cookTime,
-                                ingredients,
-                                recipe.method,
-                                recipe.referenceUrl,
-                                recipe.imageUrl,
-                                recipe.lastUpdated,
-                                recipe.serves,
-                                recipe.makes
-                        )
+                        getRecipeModelFromResponse(recipeCache.data)
                 )
             }
         }
 
         if (recipeCache.isExpiring()) {
             request.subscribe()
-            val recipe = recipeCache.data
-            val ingredients = ArrayList<RecipeModel.Ingredient>()
-
-            recipe.ingredientSections.forEach {
-                if (!it.heading.isNullOrBlank()) {
-                    ingredients.add(RecipeModel.Heading(LookupIngredientType.Heading.id, it.heading))
-                }
-            }
-
             return Single.create {
                 it.onSuccess(
-                        RecipeModel(
-                                recipe.id,
-                                recipe.title,
-                                recipe.prepTime,
-                                recipe.cookTime,
-                                ingredients,
-                                recipe.method,
-                                recipe.referenceUrl,
-                                recipe.imageUrl,
-                                recipe.lastUpdated,
-                                recipe.serves,
-                                recipe.makes
-                        )
+                        getRecipeModelFromResponse(recipeCache.data)
                 )
             }
         }
