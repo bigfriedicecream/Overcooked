ac1ad97b8935dcb31ea64dcc59441413f1edfe73
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-05-04 13:10:02 +1000

    #13 moved recipe model to manager

--- Code/app/src/main/java/com/twobrothers/overcooked/app/RecipeManager.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/app/RecipeManager.kt
@@ -5,8 +5,14 @@ import io.reactivex.Single
 
 object RecipeManager {
 
+    var recipeModel: RecipeModel? = null
+        private set
+
     fun getRecipe(id: String): Single<RecipeModel> {
         return ApiClient.getRecipe(id)
+                .doOnSuccess {
+                    recipeModel = it
+                }
     }
 
 }
--- Code/app/src/main/java/com/twobrothers/overcooked/presenters/RecipePresenter.kt
+++ Code/app/src/main/java/com/twobrothers/overcooked/presenters/RecipePresenter.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import com.twobrothers.overcooked.app.RecipeManager
 import com.twobrothers.overcooked.interfaces.IRecipeContract
 import com.twobrothers.overcooked.lookups.LookupIngredientType
-import com.twobrothers.overcooked.models.recipe.RecipeModel
 import com.twobrothers.overcooked.views.recipe.IngredientViewAdapter
 import com.twobrothers.overcooked.views.recipe.MethodViewAdapter
 import io.reactivex.disposables.CompositeDisposable
@@ -14,17 +13,18 @@ import io.reactivex.rxkotlin.subscribeBy
 class RecipePresenter(private val view:IRecipeContract.View) : IRecipeContract.Presenter {
 
     private val disposable = CompositeDisposable()
-    private var recipeModel: RecipeModel? = null
 
     override fun onStart(args: Bundle?) {
         RecipeManager
                 .getRecipe(args!!.getString("id"))
                 .subscribeBy(
                         onSuccess = {
-                            recipeModel = it
-                            view.render(it)
-                            view.onMethodDataSetChanged()
-                            view.onIngredientDataSetChanged()
+                            val r = RecipeManager.recipeModel
+                            if (r != null) {
+                                view.render(r)
+                                view.onMethodDataSetChanged()
+                                view.onIngredientDataSetChanged()
+                            }
                         },
                         onError =  { it.printStackTrace() }
                 )
@@ -44,31 +44,31 @@ class RecipePresenter(private val view:IRecipeContract.View) : IRecipeContract.P
     }
 
     override fun onBindMethodRepositoryRowViewAtPosition(holder: MethodViewAdapter.Holder, position: Int) {
-        val recipeModel = recipeModel
+        val recipeModel = RecipeManager.recipeModel
         recipeModel ?: return
         holder.render(position + 1, recipeModel.method[position])
     }
 
     override fun getMethodRepositoriesRowsCount(): Int {
-        val recipeModel = recipeModel
+        val recipeModel = RecipeManager.recipeModel
         recipeModel ?: return 0
         return recipeModel.method.size
     }
 
     override fun onBindIngredientRepositoryRowViewAtPosition(holder: IngredientViewAdapter.Holder, position: Int) {
-        val recipeModel = recipeModel
+        val recipeModel = RecipeManager.recipeModel
         recipeModel ?: return
         holder.render(recipeModel.ingredients[position], recipeModel.food)
     }
 
     override fun getIngredientRepositoriesRowsCount(): Int {
-        val recipeModel = recipeModel
+        val recipeModel = RecipeManager.recipeModel
         recipeModel ?: return 0
         return recipeModel.ingredients.size
     }
 
     override fun getIngredientItemViewType(position: Int): Int {
-        val recipeModel = recipeModel
+        val recipeModel = RecipeManager.recipeModel
         recipeModel ?: return -1
 
         val ingredient = recipeModel.ingredients[position]
