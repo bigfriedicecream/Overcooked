9edda742bf853e3dabeda1980a77a9f8571b5789
bigfriedicecream <michaeldsouza.md@gmail.com>
2020-01-02 18:32:43 +1100

    added interactive loading state

--- app/src/main/java/com/twobrothers/overcooked/interactive/InteractiveViewModel.kt
+++ app/src/main/java/com/twobrothers/overcooked/interactive/InteractiveViewModel.kt
@@ -1,5 +1,7 @@
 package com.twobrothers.overcooked.interactive
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.twobrothers.overcooked.core.datasource.FirebaseApiDataSource
@@ -12,17 +14,43 @@ class InteractiveViewModel(id: String) : ViewModel() {
     private val dataSource: FirebaseApiDataSource =
         DaggerFirebaseApiDataSourceComponent.create().getDataSource()
 
+    //region state properties
+
+    private val _isInteractiveVisible = MutableLiveData<Boolean>()
+    val isInteractiveVisible: LiveData<Boolean> = _isInteractiveVisible
+
+    private val _isLoadingIndicatorVisible = MutableLiveData<Boolean>()
+    val isLoadingIndicatorVisible: LiveData<Boolean> = _isLoadingIndicatorVisible
+
+    //endregion
+
     init {
+        showLoadingIndicator()
         viewModelScope.launch {
             val recipeList = dataSource.getRecipe(id)
             if (recipeList != null) {
                 handleSuccess(recipeList)
+                showInteractive()
             }
         }
     }
 
-    private fun handleSuccess(recipe: Recipe) {
+    //region private
 
+    private fun handleSuccess(recipe: Recipe) {
+        println(recipe)
     }
+
+    private fun showInteractive() {
+        _isLoadingIndicatorVisible.value = false
+        _isInteractiveVisible.value = true
+    }
+
+    private fun showLoadingIndicator() {
+        _isInteractiveVisible.value = false
+        _isLoadingIndicatorVisible.value = true
+    }
+
+    //endregion
 
 }
