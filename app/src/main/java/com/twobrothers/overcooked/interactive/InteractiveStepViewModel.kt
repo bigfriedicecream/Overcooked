b844d3040cf1fb85754e9e1234f7079a959c3dcb
bigfriedicecream <michaeldsouza.md@gmail.com>
2020-02-01 13:23:20 +1100

    created countdown timer

--- app/src/main/java/com/twobrothers/overcooked/interactive/InteractiveStepViewModel.kt
+++ app/src/main/java/com/twobrothers/overcooked/interactive/InteractiveStepViewModel.kt
@@ -1,5 +1,6 @@
 package com.twobrothers.overcooked.interactive
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -27,16 +28,45 @@ class InteractiveStepViewModel(step: InteractiveStep, serves: Int) : ViewModel()
 
     //endregion
 
+    private var timer: CountDownTimer = object : CountDownTimer(0, 0) {
+        override fun onFinish() {}
+        override fun onTick(millisUntilFinished: Long) {}
+    }
+
     init {
         _title.value = step.title
         _body.value = step.body
         _ingredients.value = Pair(step.ingredients, serves)
         _footnote.value = step.footnote
-        _timerDisplay.value = step.timer?.seconds.toString()
+
+        if (step.timer != null) {
+            setTimer(5000)
+            updateTimerDisplay(5000)
+        }
     }
 
-    //region private
+    // region private
 
+    private fun setTimer(milliseconds: Long) {
+        timer = object : CountDownTimer(milliseconds, 1000) {
+            override fun onFinish() {}
+            override fun onTick(millisUntilFinished: Long) {
+                updateTimerDisplay(millisUntilFinished)
+            }
+        }
+    }
+
+    private fun updateTimerDisplay(millisUntilFinished: Long) {
+        _timerDisplay.value = (millisUntilFinished / 1000).toString()
+    }
+
+    // endregion
+
+    //region public
+
+    fun startTimer() {
+        timer.start()
+    }
 
     //endregion
 
